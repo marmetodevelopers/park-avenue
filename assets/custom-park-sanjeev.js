9e2cb1333ebfbd1ce959f2a04c18510b2618cdc3
MTechSanjeev <sanjeev.k@marmeto.com>
2024-02-27 18:47:33 +0530

    removed the script that is not required

--- assets/custom-park-sanjeev.js
+++ assets/custom-park-sanjeev.js
@@ -1,41 +1,3 @@
-
-
-class ParkTrendingCarousel extends HTMLElement {
-    constructor() {
-        super();
-
-        this.id = this.getAttribute('id');
-
-        document.addEventListener('DOMContentLoaded', () => {
-            new Splide(`#${this.id}`, {
-                type: 'slide',
-                rewind: true,
-                rewindSpeed: 1000,
-                start: 1,
-                perPage: 3,
-                classes: {
-                    pagination: 'splide__pagination ibc_pagination',
-                    page: 'splide__pagination__page ibc_page',
-                },
-                breakpoints: {
-                    749: {
-                        perPage: 1,
-                        arrows: true,
-                        pagination: true
-                    },
-                },
-                gap: '79px',
-                arrows: false,
-                pagination: false
-            }).mount();
-        });
-
-    }
-
-}
-
-customElements.define('park-trending-carousel', ParkTrendingCarousel);
-
 class SplideCarousel extends HTMLElement {
     constructor() {
         super();
@@ -47,9 +9,6 @@ class SplideCarousel extends HTMLElement {
         this.arrowMobile = Boolean(this.dataset.mobileArrow);
         this.paginationDesktop = Boolean(this.dataset.desktopPagination);
         this.paginationMobile = Boolean(this.dataset.mobilePagination);
-
-        console.log(typeof(this.paginationDesktop), typeof(this.paginationMobile), typeof(this.arrowDesktop), typeof(this.arrowMobile), this.gapValue)
-
     }
     connectedCallback() {
         new Splide(`#${this.id}`, {
@@ -59,8 +18,8 @@ class SplideCarousel extends HTMLElement {
             start: 1,
             perPage: this.perPageDesktop,
             gap: `${this.gapValue}px`,
-            arrows: Boolean(this.arrowDesktop),
-            pagination: Boolean(this.paginationDesktop),
+            arrows: this.arrowDesktop,
+            pagination: this.paginationDesktop,
             classes: {
                 pagination: 'splide__pagination ibc_pagination',
                 page: 'splide__pagination__page ibc_page',
@@ -74,7 +33,6 @@ class SplideCarousel extends HTMLElement {
             }
         }).mount();
     }
-
 }
 
 customElements.define('splide-carousel', SplideCarousel);
